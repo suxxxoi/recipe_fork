18a05cd7ebaf4b1310d04b27eab745c3ea337d50
simon <simon.sattlecker@gmx.at>
2020-12-01 14:35:59 +0100

    Fixed bug with displayed_user and user.

--- recipe_fork/user/urls.py
+++ recipe_fork/user/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'user'
 urlpatterns = [
     path('', views.IndexView, name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='user'),
+    # path('<int:pk>/', views.DetailView.as_view(), name='user'),
+    path('<int:pk>/', views.detail, name='user'),
     path('<int:user_pk>/r/', include('recipe.urls'), name='recipe'),
 ]
--- recipe_fork/user/views.py
+++ recipe_fork/user/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.views import generic
 from django.urls import reverse
@@ -17,3 +17,10 @@ def IndexView(request):
 class DetailView(generic.DetailView):
     model = User
     template_name = 'user/detail.html'
+
+def detail(request, pk):
+#def detail(request, displayed_user_id):
+    response = 'Test response'
+    displayed_user = get_object_or_404(User, pk=pk)
+    return render(request, 'user/detail.html', {'displayed_user':  displayed_user})
+
